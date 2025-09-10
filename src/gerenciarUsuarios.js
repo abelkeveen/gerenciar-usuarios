c8c8f758bc8f9356e472f159fdb4297ba90de30c
Abelkeveen <aabelkeveenn@gmail.com>
2025-09-09 22:58:38 -0300

    Iniciando com objetos no JavaScript

--- src/gerenciarUsuarios.js
+++ src/gerenciarUsuarios.js
@@ -1,14 +1,36 @@
-const usuarios = ["abel", "jully", "raimundo", "lili"];
+// Criando uma variavel em fila
+const usuarios = [// Objeto
+  {
+    nome: "Abel",// Propriedade
+    email: "abel@gmail.com",
+  },
+  {
+    nome: "Jully",
+    email: "jully@linda.com",
+  },
+  { 
+    nome: "Lili", 
+    email: "lili@gmail.com" 
+  },
+  { 
+    nome: "raimundo", 
+    email: "raimundo@gmail.com" },
+];
+// Comando para contar valores de uma lista
+usuarios.length;
 
+//Criando um função para retornar a variavel
 function retornarUsuarios() {
   return usuarios;
 }
 
-function adicionarNovoUsuario(nome) {
-  usuarios.push(nome);
+//Criando uma função para adicionar usuários na fila
+function adicionarNovoUsuario(usuario) {
+  usuarios.push(usuario);
 }
 
+//Exportando a função para o arquivo gerenciarUsuarios
 module.exports = {
   retornarUsuarios,
-  adicionarNovoUsuario
+  adicionarNovoUsuario,
 };
--- test/gerenciarUsuarios.test.js
+++ test/gerenciarUsuarios.test.js
@@ -1,3 +1,4 @@
+// Importação de funções
 const {
   retornarUsuarios,
   adicionarNovoUsuario,
@@ -8,13 +9,23 @@ const { expect } = require("chai");
 //grupo de teste
 describe("Testar as funções de Gestão de Usuários", function () {
   it("Validar que posso adicionar um novo usuário na lista", function () {
-    //1. Adicionar um novo nome na lista de usuários
-    adicionarNovoUsuario("maria");
+    // 1. Adicionar um novo nome na lista de usuários
+    adicionarNovoUsuario({
+      nome: "maria", 
+      email: "maria@gmail.com"
+    });
 
-    //2. Retornar a lista de usuários na caixa lista de usuários
+    // 2. Retornar a lista de usuários na caixa lista de usuários
     const listaDeUsuarios = retornarUsuarios();
 
-    //3. Comparar se o novo nome está no fim da lista de usuários
-    expect(listaDeUsuarios.at(-1)).to.equal("maria");
+    // 3. Comparar se o novo nome está no fim da lista de usuários
+    expect(listaDeUsuarios.at(-1).nome).to.equal("maria"); 
+    // at(-1) vai pegar o último objeto adicionado | (.nome) vai pegar a propriedade nome e comparar
+    expect(listaDeUsuarios.at(-1).email).to.equal("maria@gmail.com"); // to.equal compara valores do objeto
+
+    expect(listaDeUsuarios.at(-1)).to.eql({ // to.eql compara o objeto
+        nome: "maria", 
+        email: "maria@gmail.com"
+    });
   });
 });
